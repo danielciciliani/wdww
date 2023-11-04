ae68654bff74c8302d9d75786656681e6ae84692
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2023-11-04 14:48:29 +0100

    fist commit, docker configuration and hello world

---
+++ public/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>wdww app</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+    <body class="container-fluid">
+        <h1>What do we watch?</h1>
+        <h4>Here you can see an app that will connect you to all the content you want to watch</h4>
+
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
+
